e8f4e9e0eb156c74084d3fd716e5888b7545279c
Olivier Aribaut <tang.olivier05@gmail.com>
2025-05-28 15:04:07 +0200

    #5 modif main

--- src/main.c
+++ src/main.c
@@ -38,6 +38,7 @@ int main(int argc, char **argv) {
   if ( strncmp( configuration.command, "first_pixel", 11 ) == 0 ) {
     /* first_pixel() function is defined in feature.h and implemented in feature.c */
     first_pixel(configuration.filenames[0]);
+  }
   if ( strncmp( configuration.command, "tenth_pixel", 11 ) == 0 ) {
     /* tenth_pixel() function is defined in feature.h and implemented in feature.c */
     tenth_pixel(configuration.filenames[0]);
